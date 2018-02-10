9d4b12620ccc388b5f93b8d05aba6ce3fab0b628
Brennon Brimhall <brennonbrimhall@gmail.com>
2018-02-10 12:02:56 -0700

    Don't use potentiometer.

--- src/org/usfirst/frc/team6844/robot/Arm.java
+++ src/org/usfirst/frc/team6844/robot/Arm.java
@@ -10,23 +10,21 @@ import org.uvstem.borg.logging.Message;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
-import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import edu.wpi.first.wpilibj.DigitalInput;
-
-
 
 public class Arm extends BorgSubsystem{
 
 	private TalonSRX armMotor = new TalonSRX(5);
-	private AnalogPotentiometer potentiometer = new AnalogPotentiometer(0);
 	private DigitalInput bottomSwitch = new DigitalInput(0);
 	private DigitalInput topSwitch = new DigitalInput(1);
 
 	private int counter = 0;
 
 	private Position position = Position.BOTTOM;
+	private Position previousPosition = Position.BOTTOM;
 
 	private final int TICKS_UP = 25;
+	private final int TICKS_DOWN = 50 - TICKS_UP;
 
 	private final double TRAVEL_UP_OUTPUT = .6;
 	private final double TRAVEL_DOWN_OUTPUT = -.4;
@@ -42,12 +40,17 @@ public class Arm extends BorgSubsystem{
 	}
 
 	public void setPosition(Position position) {
+		if (position != this.position) {
+			this.previousPosition = this.position;
 			this.position = position;
+		}
 	}
 
 	public void update() {
 			switch (position) {
 				case TOP:
+					counter = 0;
+					
 					if (!topSwitch.get()) {
 						armMotor.set(ControlMode.PercentOutput, TRAVEL_UP_OUTPUT);
 					} else {
@@ -55,19 +58,32 @@ public class Arm extends BorgSubsystem{
 					}
 					break;
 
-				case MIDDLE:
-					if (counter < TICKS_UP) {
-						armMotor.set(ControlMode.PercentOutput, TRAVEL_UP_OUTPUT);
+				case MIDDLE:					
+					if (previousPosition == Position.BOTTOM) {
+						if (counter < TICKS_UP) {
+							armMotor.set(ControlMode.PercentOutput, TRAVEL_UP_OUTPUT);
+						} else {
+							armMotor.set(ControlMode.PercentOutput, MIDDLE_HOLDING_OUTPUT);
+						}
+						break;
 					} else {
-						armMotor.set(ControlMode.PercentOutput, MIDDLE_HOLDING_OUTPUT);
+						if (counter < TICKS_DOWN) {
+							armMotor.set(ControlMode.PercentOutput, TRAVEL_DOWN_OUTPUT);
+						} else {
+							armMotor.set(ControlMode.PercentOutput, MIDDLE_HOLDING_OUTPUT);
+						}
 					}
+					
+					counter++;
 					break;
 
 				case BOTTOM:
+					counter = 0;
+					
 					if (!bottomSwitch.get()) {
 						armMotor.set(ControlMode.PercentOutput, TRAVEL_DOWN_OUTPUT);
 					} else {
-						armMotor.set(ControlMode.PercentOutput, TRAVEL_DOWN_OUTPUT);
+						armMotor.set(ControlMode.PercentOutput, BOTTOM_HOLDING_OUTPUT);
 					}
 					break;
 				}
